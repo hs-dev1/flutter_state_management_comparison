5ebaf497edf6d57efd5d4aa06aa2ea915328734d
hussnain <mhussnainshabbir123@gmail.com>
2024-09-26 17:20:08 +0500

    level 2 of counter incerment and decrement and reset action witht the state of counter and adding history.

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,20 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
 
 void main() {
-  final store = Store<int>(
+  final store = Store<AppState>(
     counterReducer,
-    initialState: 0,
+    initialState: AppState(counter: 0, history: []),
   );
   runApp(MyApp(store: store));
 }
 
 class MyApp extends StatelessWidget {
-  final Store<int> store;
+  final Store<AppState> store;
   const MyApp({super.key, required this.store});
 
   @override
   Widget build(BuildContext context) {
-    return StoreProvider<int>(
+    return StoreProvider<AppState>(
       store: store,
       child: const MaterialApp(
         home: MyHomePage(),
@@ -27,8 +27,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-
-  // int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -41,14 +39,30 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            StoreConnector<int, String>(
-              converter: (store) => store.state.toString(),
+            StoreConnector<AppState, String>(
+              converter: (store) => store.state.counter.toString(),
               builder: (context, count) {
                 return Text(
                   count,
                   style: Theme.of(context).textTheme.headlineMedium,
                 );
               },
+            ),
+            const SizedBox(height: 12),
+            StoreConnector<AppState, List<int>>(
+              builder: (context, history) {
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: history.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text('Value: ${history[index]}'),
+                      );
+                    },
+                  ),
+                );
+              },
+              converter: (store) => store.state.history,
             ),
           ],
         ),
@@ -57,15 +71,21 @@ class MyHomePage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: () => StoreProvider.of<int>(context).dispatch(IncrementAction()),
+            onPressed: () => StoreProvider.of<AppState>(context).dispatch(IncrementAction()),
             tooltip: 'Increment',
             child: const Icon(Icons.add),
           ),
           const SizedBox(height: 10),
           FloatingActionButton(
-            onPressed: () => StoreProvider.of<int>(context).dispatch(DecrementAction()),
+            onPressed: () => StoreProvider.of<AppState>(context).dispatch(DecrementAction()),
             tooltip: 'Decrement',
             child: const Icon(Icons.remove),
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () => StoreProvider.of<AppState>(context).dispatch(ResetAction()),
+            tooltip: 'Reset',
+            child: const Icon(Icons.restore_outlined),
           ),
         ],
       ),
@@ -73,15 +93,37 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+// Action Classes
 class IncrementAction {}
 
 class DecrementAction {}
 
-int counterReducer(int state, dynamic action) {
+class ResetAction {}
+
+// App State
+class AppState {
+  final int counter;
+  final List<int> history;
+
+  AppState({required this.counter, required this.history});
+}
+
+AppState counterReducer(AppState state, dynamic action) {
   if (action is IncrementAction) {
-    return state + 1;
+    return AppState(
+      counter: state.counter + 1,
+      history: [state.counter + 1, ...state.history],
+    );
   } else if (action is DecrementAction) {
-    return state - 1;
+    return AppState(
+      counter: state.counter - 1,
+      history: [state.counter - 1, ...state.history],
+    );
+  } else if (action is ResetAction) {
+    return AppState(
+      counter: 0,
+      history: [],
+    );
   }
   return state;
 }
